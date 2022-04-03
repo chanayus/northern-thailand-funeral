df3b22ab7a0c114676e1c493ef04cf2d4158713f
chanayus <chanayus1526@hotmail.com>
2022-04-03 21:18:10 +0700

    update index page

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,27 @@
 import Link from "next/link"
+import { useEffect } from "react"
 import { useRouter } from "next/router"
 
 const Home = () => {
   const router = useRouter()
+  useEffect(() => {
+    window.scroll(0, 0)
+  }, [])
   return (
-    <>
-      <div className="w-full h-screen flex justify-center items-center">
-        <button
-          className="bg-gray-300 py-3 px-10 rounded font-bold"
-          onClick={() => router.replace("/section2", "/", { shallow: true, scroll: false })}
-        >
-          Start
-        </button>
-      </div>
-    </>
+    <div className="relative">
+      <video width="100%" height="100%" autoPlay muted loop>
+        <source src="/video/bg.mp4" type="video/mp4"></source>
+      </video>
+
+      <img src="/images/logo.png" alt="" width="320" draggable={false} className="w-[35vw] absolute top-[2%] left-1/2 translate-x-[-50%]" />
+
+      <button
+        className="bg-white text-black text-2xl py-3 px-10 rounded font-bold absolute left-1/2 translate-x-[-30%] bottom-[1%]"
+        onClick={() => router.replace("/section2", "/", { shallow: true, scroll: false })}
+      >
+        Start
+      </button>
+    </div>
   )
 }
 
