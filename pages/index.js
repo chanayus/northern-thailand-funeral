ba94462b1c8efa3b8d3b391aa10668ff292a052a
chanayus <chanayus1526@hotmail.com>
2022-04-05 21:26:04 +0700

    build error fix

--- pages/index.js
+++ pages/index.js
@@ -16,10 +16,10 @@ const Home = () => {
       <div className="w-full absolute top-[40%] text-center flex justify-center flex-col items-center">
         <h2 className="text-5xl text-black font-bold mb-1">Scroll</h2>
         <svg xmlns="http://www.w3.org/2000/svg" width="60.686" height="25.734" viewBox="0 0 60.686 25.734">
-          <line id="Line_13" data-name="Line 13" x2="27" y2="25" transform="translate(0.34 0.367)" fill="none" stroke="#000" stroke-width="1" />
-          <line id="Line_15" data-name="Line 15" x2="27" y2="25" transform="translate(7.34 0.367)" fill="none" stroke="#000" stroke-width="1" />
-          <line id="Line_14" data-name="Line 14" x1="26" y2="25" transform="translate(27.34 0.367)" fill="none" stroke="#000" stroke-width="1" />
-          <line id="Line_16" data-name="Line 16" x1="26" y2="25" transform="translate(34.34 0.367)" fill="none" stroke="#000" stroke-width="1" />
+          <line id="Line_13" data-name="Line 13" x2={27} y2={25} transform="translate(0.34 0.367)" fill="none" stroke="#000" strokeWidth={1} />
+          <line id="Line_15" data-name="Line 15" x2={27} y2={25} transform="translate(7.34 0.367)" fill="none" stroke="#000" strokeWidth={1} />
+          <line id="Line_14" data-name="Line 14" x1={26} y2={25} transform="translate(27.34 0.367)" fill="none" stroke="#000" strokeWidth={1} />
+          <line id="Line_16" data-name="Line 16" x1={26} y2={25} transform="translate(34.34 0.367)" fill="none" stroke="#000" strokeWidth={1} />
         </svg>
       </div>
       <img src="/images/logo.png" alt="" width="320" draggable={false} className="w-[35vw] absolute top-[2%] left-1/2 translate-x-[-50%]" />
