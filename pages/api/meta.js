25f4272565719c6f2996051545c9184d19a63ae6
chanayus <chanayus1526@hotmail.com>
2022-04-24 12:58:13 +0700

    update meta

--- pages/api/meta.js
+++ pages/api/meta.js
@@ -1,7 +1,7 @@
 export default async function handler(req, res) {
   res.json({
     name: "เมี้ยนมลาย",
-    image: "https://northern-thailand-funeral.vercel.app/images/logo_bg.png",
+    image: "https://northern-thailand-funeral.vercel.app/images/bg-meta.jpg",
     detail: "Website เล่าเรื่องเกี่ยวกับงานศพที่สามารถให้ผู้เข้าชมมีส่วนร่วมในพิธีกรรมในงานศพ",
   })
 }
