7e27dcbc480338bf56f2c37ed0f05f299571ad91
chanayus <chanayus1526@hotmail.com>
2022-04-24 11:31:44 +0700

    Create meta.js

---
+++ pages/api/meta.js
@@ -0,0 +1,7 @@
+export default async function handler(req, res) {
+  res.json({
+    name: "เมี้ยนมลาย",
+    image: "https://www.mecallapi.com/attractions/4.jpg",
+    detail: "Website เล่าเรื่องเกี่ยวกับงานศพที่สามารถให้ผู้เข้าชมมีส่วนร่วมในพิธีกรรมในงานศพ",
+  })
+}
