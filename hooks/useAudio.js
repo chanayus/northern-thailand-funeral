cdbf0f726ca124aa2a8590c32a66db26e44650b4
chanayus <chanayus1526@hotmail.com>
2022-04-26 21:12:36 +0700

    Update useAudio.js

--- hooks/useAudio.js
+++ hooks/useAudio.js
@@ -23,12 +23,12 @@ export const useAudio = (url, isLoop = true) => {
   }
 
   const mute = () => {
-    !isMute ? audio.fade(0.1, 0, 500) : audio.fade(0, 0.1, 500)
+    !isMute ? audio.fade(0.07, 0, 500) : audio.fade(0, 0.07, 500)
     setIsMute(!isMute)
   }
 
   useEffect(() => {
-    setAudio(new Howl({ src: url, volume: 0.1, loop: isLoop, mute: isMute }))
+    setAudio(new Howl({ src: url, volume: 0.07, loop: isLoop, mute: isMute }))
   }, [])
 
   return [playing, play, pause, stop, mute, isMute]
